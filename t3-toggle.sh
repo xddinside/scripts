83a5b7d94e73b7d2503f85ecf7f91dda171ed14d
Nirvaan Salian <nirvaansaalian@gmail.com>
2025-12-02 20:12:54 +0530

    feat: add flacmenu script for music playback and update t3-toggle.sh to use helium-browser

--- t3-toggle.sh
+++ t3-toggle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if ChatGPT is already running
-if hyprctl clients | grep -q "chrome-t3.chat"; then
+if hyprctl clie
     # If running, toggle the special workspace
     hyprctl dispatch togglespecialworkspace t3
 else
     # If not running, launch it
-    chromium --new-window --app="https://t3.chat/" --class=Chromium-t3 &
+    helium-browser --new-window --app="https://t3.chat/" --class=Chromium-t3 --force-device-scale-factor=1.3 &
     sleep 1
     # Move to special workspace after launch
     hyprctl dispatch movetoworkspace special:t3
