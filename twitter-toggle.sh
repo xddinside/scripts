55943eb27694642ea4b020ea1dd3609a444ddfed
Nirvaan Salian <nirvaansaalian@gmail.com>
2026-02-23 23:45:46 +0530

    add twitter toggle script

---
+++ twitter-toggle.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CLASS="chrome-x.com__compose_post-Default"
+WORKSPACE="special:x"
+URL="https://x.com/compose/post"
+
+# Get window address by class
+get_window_addr() {
+    hyprctl clients -j | jq -r ".[] | select(.class == \"$CLASS\") | .address" | head -n1
+}
+
+window_addr=$(get_window_addr)
+
+if [[ -n "$window_addr" ]]; then
+    # Window exists, toggle the special workspace
+    hyprctl dispatch togglespecialworkspace x
+else
+    # Launch the webapp
+    chromium --new-window --app="$URL" &
+    
+    # Wait for window to appear (poll instead of fixed sleep)
+    for i in {1..50}; do
+        sleep 0.1
+        window_addr=$(get_window_addr)
+        if [[ -n "$window_addr" ]]; then
+            break
+        fi
+    done
+    
+    # Move only this specific window to the special workspace
+    if [[ -n "$window_addr" ]]; then
+        hyprctl dispatch movetoworkspacesilent "$WORKSPACE,address:$window_addr"
+        hyprctl dispatch togglespecialworkspace x
+    fi
+fi
+
